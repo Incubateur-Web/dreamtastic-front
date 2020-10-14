4da817f8c9b9e2077aa8df43708dcf5cde3e3363
Jérémy Gendre <jgendre@media-sante.com>
2020-10-14 17:58:19 +0200

    remove useless element

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ const HomePage = lazy(() => import("./pages/HomePage"));
 
 export default function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Suspense fallback={<>loading...</>}>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<>loading...</>}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
 }
