74c41f74c4a2ba7d607975176d3585e618000132
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-28 11:15:09 +0100

    initial admin setup : add link to profile

--- src/components/header/HeaderProfileSubmenu.tsx
+++ src/components/header/HeaderProfileSubmenu.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCog, faSignOutAlt, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCog,
+  faSignOutAlt,
+  faUser,
+  faUserTie,
+} from "@fortawesome/free-solid-svg-icons";
 
 type Props = {
   visible: boolean;
@@ -19,6 +24,9 @@ export default function HeaderProfileSubmenu({
     >
       <div className="profile-submenu-link px-5 w-full py-2 hover:bg-white transition duration-150 hover:text-gray-800">
         <FontAwesomeIcon className="mr-1" icon={faUser} /> Mon compte
+      </div>
+      <div className="profile-submenu-link px-5 w-full py-2 hover:bg-white transition duration-150 text-orange-500">
+        <FontAwesomeIcon className="mr-1" icon={faUserTie} /> Administration
       </div>
       <div className="profile-submenu-link px-5 py-2 hover:bg-white transition duration-150 hover:text-gray-800">
         <FontAwesomeIcon className="mr-1" icon={faCog} /> Paramètres
