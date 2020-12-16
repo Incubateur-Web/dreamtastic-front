4da20a251110b6f1a3b2093ce1298fde7ab3758f
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-12-16 16:03:24 +0100

    profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -4,6 +4,8 @@ import { useLocation, useRouteMatch } from "react-router";
 import { generateUser } from "../mocks/User";
 import { User } from "../types/API/UserType";
 import Loader from "../components/Loader";
+import { faUserEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ProfilePage() {
   const { params } = useRouteMatch<{ id: string }>();
@@ -15,6 +17,7 @@ export default function ProfilePage() {
     if (!user) {
       setUser(generateUser());
     }
+    console.log(user);
   }, [params, user]);
 
   if (user === undefined) {
@@ -27,7 +30,38 @@ export default function ProfilePage() {
 
   return (
     <DefaultLayout>
-      <div>{user.username}</div>
+      <div className="bg-gray-300 rounded text-black w-full py-4 px-8">
+        <div className="text-2xl w-full border-b-1 border-gray-400 pb-2">
+          {user.firstName} {user.lastName}
+          <FontAwesomeIcon className="ml-2 cursor-pointer" icon={faUserEdit} />
+        </div>
+        <table className="mt-4 text-left">
+          <tr>
+            <th className="py-2">Création</th>
+            <td className="px-4">{user.createdAt.toDateString()}</td>
+          </tr>
+          <tr>
+            <th className="py-2">Dernière modifications</th>
+            <td className="px-4">{user.updatedAt.toDateString()}</td>
+          </tr>
+          <tr>
+            <th className="py-2">Dernière connexion</th>
+            <td className="px-4">{user.lastConnection.toDateString()}</td>
+          </tr>
+          <tr>
+            <th className="py-2">Nom d'utilisateur</th>
+            <td className="px-4">{user.username}</td>
+          </tr>
+          <tr>
+            <th className="py-2">Description</th>
+            <td className="px-4 text-justify">{user.description}</td>
+          </tr>
+          <tr>
+            <th className="py-2">Nombre de parutions</th>
+            <td className="px-4">{user.dreams.length}</td>
+          </tr>
+        </table>
+      </div>
     </DefaultLayout>
   );
 }
