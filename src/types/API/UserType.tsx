643b1944222a8e2237f2fe5976ce4d5c0b83c81d
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-12-16 15:11:39 +0100

    add lastname and firstname to users

--- src/types/API/UserType.tsx
+++ src/types/API/UserType.tsx
@@ -2,6 +2,8 @@ import { BaseTable } from "./BaseType";
 import { Dream } from "./DreamType";
 
 export type User = BaseTable<{
+  firstName: string;
+  lastName: string;
   username: string;
   description: string;
   lastConnection: Date;
