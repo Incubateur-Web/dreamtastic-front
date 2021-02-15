c68b28c9cc01790130048108a32dbd9fd6bbd3d2
Martin PELCAT <martinpelcat@hotmail.com>
2021-02-15 11:38:59 +0100

    aaa

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,8 +1,11 @@
+import axios from "axios";
+import { addMinutes, differenceInMinutes, isBefore } from "date-fns";
 import React, {
   createContext,
   Dispatch,
   FC,
   SetStateAction,
+  useCallback,
   useEffect,
   useState,
 } from "react";
@@ -34,15 +37,47 @@ export const UserContextProvider: FC = ({ children }) => {
     return JSON.parse(tokens) as LocalStorageTokens;
   });
 
+  const getNewAccessToken = useCallback(async () => {
+    try {
+      console.log("get new AccessToekn");
+
+      const { data } = await axios.post("/auth/accessToken", {
+        refresh_token: tokens.refresh_token,
+      });
+      setTokens((token) => ({
+        ...token,
+        access_token: data.access_token,
+        expires: addMinutes(new Date(), 58),
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  }, [tokens.refresh_token]);
+
   useEffect(() => {
-    console.log("UseEffect");
-    console.log(tokens);
+    const expireDate = new Date(tokens.expires || new Date());
+    const currentDate = new Date();
+    if (isBefore(expireDate, currentDate)) {
+      getNewAccessToken();
+      return;
+    }
+    const diffMinutes = Math.abs(differenceInMinutes(new Date(), expireDate));
+
+    const timeout = setTimeout(() => {
+      console.log("timeout");
+
+      getNewAccessToken();
+    }, diffMinutes * 60 * 1000);
 
     if (tokens.access_token) {
       // fetchUser
     }
     localStorage.setItem(LOCALSTORAGE_TOKEN_KEY, JSON.stringify(tokens));
-  }, [tokens]);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [tokens, getNewAccessToken]);
 
   return (
     <UserContext.Provider value={{ user, setUser, setTokens }}>
