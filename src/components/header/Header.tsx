39e6797014754368bb9a369db392da3328f5a3ad
Jérémy Gendre <gendrejeremy@yahoo.fr>
2021-03-10 12:01:38 +0100

    remove old header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -52,28 +52,4 @@ export default function Header() {
       </div>
     </div>
   );
-
-  return (
-    <div className="bg-white bg-opacity-20 block md:flex text-lg sticky top-0 text-white z-50">
-      <div className="relative flex-1">
-        <div className="flex justify-between">
-          <div className="px-4 py-4">
-            <span className="font-pacifico text-3xl">Dreamtastic</span>
-          </div>
-          <div className="flex pr-4 justify-center items-center space-x-2">
-            {user ? (
-              <HeaderContextProvider>
-                <HeaderLogged />
-              </HeaderContextProvider>
-            ) : (
-              <HeaderSignIn />
-            )}
-          </div>
-        </div>
-        <div className="flex justify-center flex-1 md:absolute top-0 left-1/2 right-1/2 bottom-0">
-          <HeaderMenu />
-        </div>
-      </div>
-    </div>
-  );
 }
