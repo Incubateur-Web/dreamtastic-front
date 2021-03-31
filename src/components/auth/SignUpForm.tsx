c864c50cf5a61968796745c404caf0bf522e18a4
Jérémy Gendre <gendrejeremy@yahoo.fr>
2021-03-31 16:32:47 +0200

    rebuild the login and sign in forms

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -6,6 +6,8 @@ import Joi from "joi";
 import { joiResolver } from "@hookform/resolvers/joi";
 import axios from "axios";
 import { ButtonLoaderIcon } from "../icons/ButtonLoaderIcon";
+import DefaultLayout from "../../layouts/DefaultLayout";
+import Button from "../button/Button";
 
 type LoginSchema = {
   email: string;
@@ -43,101 +45,110 @@ export default function SignUpForm() {
   };
 
   return (
-    <form
-      noValidate
-      className="p-4 space-y-6 text-white w-full"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-col">
-        <label htmlFor="email" className="px-1 text-lg">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="bg-white bg-opacity-20 placeholder-white rounded-md overflow-hidden text-2xl p-2 focus:outline-none"
-          placeholder="Email"
-          autoFocus
-          ref={register}
-        />
-        {errors.email && (
-          <span className="text-gray-200 text-sm">{errors.email.message}</span>
-        )}
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="email" className="px-1 text-lg">
-          Nom
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="bg-white bg-opacity-20 placeholder-white rounded-md overflow-hidden text-2xl p-2 focus:outline-none"
-          placeholder="Nom"
-          ref={register}
-        />
-        {errors.name && (
-          <span className="text-gray-200 text-sm">{errors.name.message}</span>
-        )}
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="password" className="px-1 text-lg">
-          Mot de passe
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className="bg-white bg-opacity-20 placeholder-white rounded-md overflow-hidden text-2xl p-2 focus:outline-none"
-          placeholder="Mot de passe"
-          ref={register}
-        />
-        {errors.password && (
-          <span className="text-gray-200 text-sm">
-            {errors.password.message}
-          </span>
-        )}
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="repeat_password" className="px-1 text-lg">
-          Confirmer le mot de passe
-        </label>
-        <input
-          type="password"
-          id="repeat_password"
-          name="repeat_password"
-          className="bg-white bg-opacity-20 placeholder-white rounded-md overflow-hidden text-2xl p-2 focus:outline-none"
-          placeholder="Mot de passe"
-          ref={register}
-        />
-        {errors.repeat_password && (
-          <span className="text-gray-200 text-sm">
-            {errors.repeat_password.message}
-          </span>
-        )}
-      </div>
-      <div className="pt-8">
-        <button
-          type="submit"
-          disabled={!formState.isValid}
-          className={clsx(
-            [!formState.isValid ? "bg-opacity-10" : "bg-opacity-20"],
-            "w-full bg-white text-3xl rounded-lg py-2 text-white font-bold focus:outline-none focus:shadow-outline flex space-x-2 justify-center items-center"
-          )}
+    <DefaultLayout>
+      <div className="flex w-full">
+        <form
+          noValidate
+          className="px-6 py-4 space-y-6 text-black mx-auto rounded-md shadow-xl"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {isLoading && <ButtonLoaderIcon />}
-          <span>S'inscrire</span>
-        </button>
-        <div className="w-full mt-2 flex flex-row-reverse justify-between px-1 text-sm">
-          <div>
-            Déjà enregistré ?{" "}
-            <Link to="/auth/signin" className="text-white hover:underline">
-              Se connecter
-            </Link>
+          <div className="text-center font-bold text-2xl">Inscription</div>
+          <div className="flex flex-col">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="bg-white bg-opacity-20 placeholder-gray-600 rounded-full border border-gray-300 focus:border-dark-violet overflow-hidden text-lg px-6 py-2 focus:outline-none"
+              placeholder="Email"
+              autoFocus
+              ref={register}
+            />
+            {errors.email && (
+              <span className="text-red-400 text-sm">
+                {errors.email.message}
+              </span>
+            )}
           </div>
-        </div>
+          <div className="flex flex-col">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="bg-white bg-opacity-20 placeholder-gray-600 rounded-full border border-gray-300 focus:border-dark-violet overflow-hidden text-lg px-6 py-2 focus:outline-none"
+              placeholder="Nom d'utilisateur"
+              ref={register}
+            />
+            {errors.name && (
+              <span className="text-red-400 text-sm">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="bg-white bg-opacity-20 placeholder-gray-600 rounded-full border border-gray-300 focus:border-dark-violet overflow-hidden text-lg px-6 py-2 focus:outline-none"
+              placeholder="Mot de passe"
+              ref={register}
+            />
+            {errors.password && (
+              <span className="text-red-400 text-sm">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              type="password"
+              id="repeat_password"
+              name="repeat_password"
+              className="bg-white bg-opacity-20 placeholder-gray-600 rounded-full border border-gray-300 focus:border-dark-violet overflow-hidden text-lg px-6 py-2 focus:outline-none"
+              placeholder="Mot de passe"
+              ref={register}
+            />
+            {errors.repeat_password && (
+              <span className="text-red-400 text-sm">
+                {errors.repeat_password.message}
+              </span>
+            )}
+          </div>
+          <div>
+            <Button
+              disabled={!formState.isValid}
+              onClick={() => {
+                console.log("tg");
+              }}
+              extraClasses={clsx(
+                [
+                  !formState.isValid
+                    ? "bg-opacity-10 hover:bg-opacity-10 cursor-default"
+                    : "",
+                ],
+                "w-full text-xl py-2 font-bold flex"
+              )}
+            >
+              {isLoading ? (
+                <ButtonLoaderIcon />
+              ) : (
+                <span className="mx-auto">S'inscrire</span>
+              )}
+            </Button>
+            <div className="w-full mt-2 flex flex-row-reverse justify-between px-1 text-sm">
+              <div>
+                Déjà enregistré ?{" "}
+                <Link
+                  to="/auth/signin"
+                  className="hover:underline text-violet hover:text-light-violet"
+                >
+                  Se connecter
+                </Link>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
+    </DefaultLayout>
   );
 }
