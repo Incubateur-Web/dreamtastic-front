68f5a18b92ac8b6a39ef37f7241d971261c37e0d
Martin PELCAT <martinpelcat@hotmail.com>
2021-04-19 10:49:49 +0200

    disable top user because backend is not set

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,5 +1,5 @@
 import { SearchUsers } from "../components/search/SearchUsers";
-import { TopUserCard } from "../components/user/TopUserCard";
+// import { TopUserCard } from "../components/user/TopUserCard";
 import DefaultLayout from "../layouts/DefaultLayout";
 import { TopBg } from "../components/background/TopBg";
 import { UserCard } from "../components/UserCard";
@@ -25,10 +25,10 @@ export default function UsersPage() {
           <SearchUsers onChange={setUsers} />
         </div>
 
-        <div className="-mx-4 px-4 flex space-x-4 z-50 flex-nowrap overflow-x-auto md:overflow-hidden py-3">
+        {/* <div className="-mx-4 px-4 flex space-x-4 z-50 flex-nowrap overflow-x-auto md:overflow-hidden py-3">
           <TopUserCard />
           <TopUserCard />
-        </div>
+        </div> */}
 
         <div className="container mx-auto space-y-4 px-3 md:px-0">
           <div className="-mx-2 flex flex-nowrap overflow-x-auto md:flex-wrap md:overflow-hidden pb-3 md:pb-0">
