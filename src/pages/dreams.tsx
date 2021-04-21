abf9e4c6890b7d3d1aa895d55558dc13281c1a83
Martin PELCAT <martinpelcat@hotmail.com>
2021-04-21 10:03:41 +0200

    disable shuffle

--- src/pages/dreams.tsx
+++ src/pages/dreams.tsx
@@ -1,6 +1,5 @@
 import { DreamPreviewCard } from "../components/dreams/DreamPreviewCard";
 import DefaultLayout from "../layouts/DefaultLayout";
-import { ShuffleIcon } from "../components/icons/ShuffleIcon";
 import {
   SearchDreams,
   SearchFormFields,
@@ -70,13 +69,13 @@ export default function DreamsPage() {
       <div className="container mx-auto space-y-11 md:space-y-20 px-3 md:px-0 z-10">
         <div className="flex flex-col md:flex-row items-center w-full space-y-4 space-x-0 md:space-x-4 md:space-y-0">
           <SearchDreams onChange={handleSearchChange} />
-          <div
+          {/* <div
             className="flex space-x-2 border-b-2 p-1 border-main z-10 font-semibold"
             role="button"
           >
             <ShuffleIcon fill="#6bdabe" />
             <span>Surprenez-moi</span>
-          </div>
+          </div> */}
         </div>
         <DreamsList query={query} />
       </div>
